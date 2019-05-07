2cbce5b1e57f8743f151b79a3b75422b7cfd3788
chaps <jean-cedric.chappelier@epfl.ch>
2019-05-07 14:21:13 +0200

    correction 1er rendu du projet

---
+++ grading/projet01/done/commands.c
@@ -0,0 +1,404 @@
+/**
+ * @file commands.c
+ * @brief Processor commands(/instructions) simulation
+ *
+ * @date 2019
+ */
+
+#include <stdio.h>
+#include <stdlib.h> 	// strtoul(), memory alloc.
+#include <inttypes.h> // PRIX macro
+#include <string.h>		// strlen()
+#include <ctype.h> 		// isspace()
+
+#include "commands.h"
+#include "addr_mng.h"
+#include "mem_access.h"
+#include "addr.h"
+#include "error.h"
+
+#define BYTE_MAX_VALUE 0xFF
+#define WORD_MAX_VALUE 0xFFFFFFFF
+
+#define INIT_COMMANDS_NB 10
+
+int program_init(program_t * program) {
+	M_REQUIRE_NON_NULL(program);
+
+	program->listing = calloc(INIT_COMMANDS_NB, sizeof(command_t));
+	M_EXIT_IF_NULL(program->listing, 10*sizeof(command_t));
+
+	program->nb_lines = 0;
+	program->allocated = sizeof(program->listing);
+
+	return ERR_NONE;
+}
+
+int program_free(program_t* program){
+	M_REQUIRE_NON_NULL(program);
+	M_REQUIRE_NON_NULL(program->listing);
+
+	free(program->listing);
+	program->listing = NULL;
+	program->nb_lines = 0;
+	program->allocated = 0;
+
+	return ERR_NONE;
+}
+
+static int program_enlarge(program_t* program);
+int program_add_command(program_t* program, command_t const * command) {
+	M_REQUIRE_NON_NULL(program);
+	M_REQUIRE_NON_NULL(command);
+
+  // correcteur can be diff than READ / WRITE
+  // corecteur can be diff than DATA / INSTRUCTION
+  // correcteur incorrect data_size is valid
+  // correcteur page offset is not alligned
+  // correcteur write_data is one when read
+
+	// Write Instr.
+	M_REQUIRE( !(command->type == INSTRUCTION && command->data_size != sizeof(word_t) ), ERR_BAD_PARAMETER, "%s", "illegal command: read should not have write data");
+	if(command->order == WRITE){
+		// data size is either word or byte
+		M_REQUIRE(command->data_size == sizeof(word_t) || command->data_size == sizeof(byte_t), ERR_SIZE, "%s", "illegal command: data size is neither word nor byte");
+
+		// data_size and write_data value are coherent
+		int write_word_coherent = (command->data_size == sizeof(word_t)) && (command->write_data <= WORD_MAX_VALUE);
+		int write_byte_coherent = (command->data_size == sizeof(byte_t)) && (command->write_data <= BYTE_MAX_VALUE);
+		M_REQUIRE( write_word_coherent || write_byte_coherent, ERR_SIZE, "%s", "illegal command: write data is too big compared to declared datasize");
+	}
+
+	if(command->type == INSTRUCTION){
+		M_REQUIRE( command->data_size == sizeof(word_t), ERR_BAD_PARAMETER, "%s", "Illegal command: Instruction data size should always be sizeof(word_t)");
+	}
+
+	// Address offset should be a multiple of data size
+	if(command->data_size == sizeof(word_t)){
+		//Should be word aligned
+		M_REQUIRE((command->vaddr.page_offset & 0x3) == 0, ERR_ADDR, "%s", "Illegal command : address should be word aligned when dealing with words");
+	}
+	// Note : if data is in bytes, every address has valid offset
+
+
+	while(program->nb_lines >= program->allocated){
+		M_EXIT_IF_ERR(program_enlarge(program), "Error trying to reallocate more memory");
+	}
+
+	program->listing[program->nb_lines] = *command;
+	(program->nb_lines)++;
+	return ERR_NONE;
+}
+
+/**
+ * @brief Reallocates twice as much memory to store commands in program
+ * @param program (modified) the program which needs more memory
+ * @return ERR_NONE if successful, ERR_MEM otherwise
+ */
+static int program_enlarge(program_t* program){
+	program_t enlarged = *program;
+	enlarged.allocated *= 2;
+	if ((enlarged.allocated > SIZE_MAX / sizeof(command_t) ||
+		(enlarged.listing = realloc(enlarged.listing, enlarged.allocated * sizeof(command_t))) == NULL)) {
+		M_EXIT(ERR_MEM, "Error trying to reallocate to %zu bytes (possible overflow)", 2 * enlarged.allocated * sizeof(command_t));
+	}
+	*program = enlarged;
+
+	return ERR_NONE;
+}
+
+int program_shrink(program_t* program) {
+	M_REQUIRE_NON_NULL(program);
+
+	program_t shrunk = *program;
+	//NOTE : Realocating DOWN cannot possibly cause an overflow
+	if(program->nb_lines <= 10){
+		M_EXIT_IF_NULL(shrunk.listing = realloc(shrunk.listing, INIT_COMMANDS_NB * sizeof(command_t)),
+ 									 INIT_COMMANDS_NB * sizeof(command_t));
+		shrunk.allocated = INIT_COMMANDS_NB;
+	}else{
+		M_EXIT_IF_NULL(shrunk.listing = realloc(shrunk.listing, shrunk.nb_lines * sizeof(command_t)),
+ 									 shrunk.nb_lines * sizeof(command_t));
+		shrunk.allocated = shrunk.nb_lines;
+	}
+
+	*program = shrunk;
+
+	return ERR_NONE;
+}
+
+// program_print helper functions' prototypes
+static void print_order(	FILE* o, command_t const * c);
+static void print_type_size(FILE* o, command_t const * c);
+static void print_data(		FILE* o, command_t const * c);
+static void print_addr(		FILE* o, command_t const * c);
+
+int program_print(FILE* output, program_t const * program) {
+	M_REQUIRE_NON_NULL(output);
+	M_REQUIRE_NON_NULL(program);
+
+	command_t c;
+
+	for(int i = 0; i < program->nb_lines; i++) {
+		c = program->listing[i];
+
+		print_order(output, &c);
+		fflush(output);
+
+		print_type_size(output, &c);
+		fflush(output);
+
+		print_data(output, &c);
+		fflush(output);
+
+		print_addr(output, &c);
+		fflush(output);
+
+		fprintf(output, "\n");
+		fflush(output);
+	}
+
+	return ERR_NONE;
+}
+
+static void print_order(FILE*  o , command_t const * c) {
+	 if(c->order == READ) fprintf(o, "R ");
+	 else fprintf(o, "W ");
+}
+
+static void print_type_size(FILE* o , command_t const * c){
+	if(c->type == INSTRUCTION) fprintf(o, "I  "); // I and two white spaces
+	else {
+		if (c->data_size == sizeof(word_t)) fprintf(o, "DW ");
+		else fprintf(o, "DB ");
+	}
+}
+
+static void print_data(FILE* o , command_t const * c) {
+	if(c->order == WRITE) {
+		if(c->data_size == sizeof(byte_t)) fprintf(o, "0x%02" PRIX32 "       ", c->write_data);
+		else fprintf(o, "0x%08" PRIX32 " ", c->write_data);
+	}
+	else fprintf(o, "           "); // 11 whites spaces
+}
+
+static void print_addr(FILE* o , command_t const * c) {
+	fprintf(o, "@0x%016" PRIX64, virt_addr_t_to_uint64_t( &(c->vaddr)) );
+}
+
+
+
+// helper function prototypes
+static int read_command_line(FILE* input, char* str, const size_t str_len);
+static int next_word(char* str, char* read, const size_t read_len, unsigned int* index);
+static int parse_order(command_t * c, char const * word);
+static int parse_type_and_size(command_t * c, char const * word);
+static int parse_data(command_t * c, char const * word);
+static int parse_address(command_t * c, char const * word);
+#define MAX_COMMAND_LENGTH 36
+#define MAX_COMMAND_WORD_LENGTH 19 // ADDRESS_CHARS
+
+int program_read(const char* filename, program_t* program){
+	M_REQUIRE_NON_NULL(filename);
+	M_REQUIRE_NON_NULL(program);
+
+	FILE* input = fopen(filename, "r");
+	M_REQUIRE_NON_NULL_CUSTOM_ERR(input, ERR_IO);
+
+	char command_str[MAX_COMMAND_LENGTH+1];
+
+	program_init(program);
+
+	/* NOTE: the underlying helper-functions only parse the input.
+	 * It doesn't check in any way that the input makes sense
+	 * Only that it has a somewhat "legal" structure (to be parseable)
+	 * Validity checks are performed in program_add_command
+	 */
+
+	while((read_command_line(input, command_str, MAX_COMMAND_LENGTH+1) != 0)
+		&& !feof(input) && !ferror(input)){
+
+		//Make a command_t of the line read
+		command_t c;
+		(void)memset(&c, 0, sizeof(c));
+
+		unsigned int index = 0;
+		char word_read[MAX_COMMAND_WORD_LENGTH+1]; //+1 accounts for '\0' in last position
+
+		M_EXIT_IF_ERR(next_word(command_str, word_read, MAX_COMMAND_WORD_LENGTH+1, &index),
+ 									"Error trying to parse instruction");
+		M_EXIT_IF_ERR(parse_order(&c, word_read), "Error trying to parse order");
+
+		M_EXIT_IF_ERR(next_word(command_str, word_read, MAX_COMMAND_WORD_LENGTH+1, &index),
+ 									"Error trying to parse instruction");
+		M_EXIT_IF_ERR(parse_type_and_size(&c, word_read),
+									"Error trying to parse type and size");
+
+		if(c.order == WRITE){
+			M_EXIT_IF_ERR(next_word(command_str, word_read, MAX_COMMAND_WORD_LENGTH+1, &index),
+										"Error trying to parse instruction");
+			M_EXIT_IF_ERR(parse_data(&c, word_read), "Error trying to parse data");
+		}
+
+		M_EXIT_IF_ERR(next_word(command_str, word_read, MAX_COMMAND_WORD_LENGTH+1, &index),
+									"Error trying to parse instruction");
+		M_EXIT_IF_ERR(parse_address(&c, word_read), "Error trying to parse address");
+
+		M_EXIT_IF_ERR(program_add_command(program, &c), "Error adding command to program");
+	}
+
+	fclose(input);
+
+	return ERR_NONE;
+}
+
+/**
+ * @brief Reads one line from the given file input
+ * @param input commands file
+ * @param str (modified) the string read
+ * @param str_len the length of the string (char array)
+ * @return the length of the string (command) read
+ */
+static int read_command_line(FILE* input, char* str, size_t str_len){
+	fgets(str, str_len, input); // correcteur could be done without gets
+
+	int len_read = strlen(str) - 1;
+
+	M_EXIT_IF(len_read < 1, ERR_IO, "%s", "Found line with only a '\n' in program input file");
+
+	if(len_read >= 0 && str[len_read] == '\n'){  //strip the '\n' off
+		str[len_read] = '\0';
+	}
+
+	return len_read;
+}
+
+/**
+ * @brief Strips one word from str, starting from index (incl.) Drops (leading) spaces.
+ * @param str string to read one word from
+ * @param read (modified) word read
+ * @param index (modified) start index, modified to index of char directly following word read
+ * @return ERR_NONE if ok, appropriate error code otherwise
+ */
+static int next_word(char* str, char* read, size_t read_len, unsigned int* index){
+	int str_len = strlen(str);
+
+	//"Drop" leading spaces, if any
+	while(*index < str_len && isspace(str[*index])){
+		(*index)++;
+	}
+
+	// reads only in cases it needs to. Empty fields are not read -> error
+	M_EXIT_IF(*index == str_len, ERR_BAD_PARAMETER, "%s",
+						"Reached end of string, but expected more (presumably wrong command format)");
+
+	// read until next space
+	size_t read_nb = 0;
+	char nextChar;
+	do{
+		M_EXIT_IF(read_nb == read_len-1, ERR_BAD_PARAMETER, "%s",
+ 							"Given 'read' char array is too small to hold next word of instruction");
+
+		nextChar = str[*index];
+		read[read_nb] = nextChar;
+		read_nb++;
+		(*index)++;
+	} while(*index < str_len && !isspace(str[*index]));
+	read[read_nb] = '\0';
+
+	return ERR_NONE;
+}
+
+#define ORDER_CHARS 1 	// I or W
+#define TYPE_CHARS 2 	// I or DB or DW
+#define DATA_CHARS 10 	// 0x + 8 hex
+#define ADDRESS_CHARS 19// @0x + 16 hex
+static int parse_order(command_t * c, char const * word) {
+	size_t word_len = strlen(word);
+	M_EXIT_IF(word_len != ORDER_CHARS, ERR_BAD_PARAMETER, "%s",
+ 						"Bad instruction format : command order should only be 1 character");
+
+	if(word[0] == 'R') {
+		c->order = READ;
+	}
+	else if (word[0] == 'W') {
+		c->order = WRITE;
+	}
+	else {
+		M_EXIT(ERR_BAD_PARAMETER, "%s",
+ 					 "Bad instruction format: first command word is not a valid order entry.");
+	}
+
+	return ERR_NONE;
+}
+static int parse_type_and_size(command_t * c, char const * word) {
+	size_t word_len = strlen(word);
+	M_EXIT_IF(word_len > TYPE_CHARS, ERR_BAD_PARAMETER, "%s",
+ 						"Bad instruction format : command type and size should only be at most 2 characters");
+
+	if (word_len == 1 && word[0] == 'I') {
+		c->type = INSTRUCTION;
+		c->data_size = sizeof(word_t);
+	}
+	else if (word_len == 2 && word[0] == 'D') {
+		c->type = DATA;
+
+		if(word[1] == 'B') {
+			c->data_size = sizeof(byte_t);
+		}
+		else if(word[1] == 'W') {
+			c->data_size = sizeof(word_t);
+		}
+		else {
+			M_EXIT(ERR_BAD_PARAMETER, "%s %c", "Bad instruction format: command type D should be followed by either B or W but was", word[1]);
+		}
+	}
+	else {
+		M_EXIT(ERR_BAD_PARAMETER, "%s %c",
+ 			"Bad instruction format: command type should be I, DB or DW but was:", word[0]);
+	}
+
+	return ERR_NONE;
+}
+
+/* unsigned long strtoul(const char *restrict str, char **restrict endptr, int base)
+ * converts a string to an unsigned long
+ * man strtoul for more information
+ */
+static int parse_data(command_t * c, char const * word) {
+	size_t word_len = strlen(word);
+	M_REQUIRE(word_len <= DATA_CHARS, ERR_BAD_PARAMETER, "%s",
+						"Bad instruction format : command data string should be at most 10 characters (\"0x\" + at most 8 HEX digits)");
+	M_REQUIRE(word[0] == '0' && word[1] == 'x', ERR_BAD_PARAMETER, "%s",
+						"Bad instruction format : data should start with prefix '0x'");
+
+	char* ptr;
+	word_t w = strtoul(word, &ptr , 0);
+	//After having read the data word, we should find a '\0'. Otherwise we stopped prematurely.
+	M_REQUIRE(*ptr == '\0', ERR_BAD_PARAMETER, "%s",
+ 						"Bad instruction format : data should only contain HEX digits");
+
+	c->write_data = w;
+
+	return ERR_NONE;
+}
+
+static int parse_address(command_t * c, char const * word) {
+	size_t word_len = strlen(word);
+	M_REQUIRE(word_len == ADDRESS_CHARS, ERR_BAD_PARAMETER, "%s",
+						"Bad instruction format : command address should take 19 chars (\"@0x\" + 16 HEX digits)");
+	M_REQUIRE(word[0] == '@' && word[1] == '0' && word[2]== 'x', ERR_BAD_PARAMETER, "%s",
+ 						"Bad instruction format : address should start with prefix '@0x'");
+
+	char* ptr;
+	//drops leading '@'
+	uint64_t a_int = strtoul(&word[1], &ptr, 16); // hex base
+	M_REQUIRE(*ptr == '\0', ERR_ADDR, "%s %c",
+						"Bad instruction format : address should end here but last char was ", *ptr);
+
+	virt_addr_t vaddr;
+	init_virt_addr64(&vaddr, a_int);
+	c->vaddr = vaddr;
+
+	return ERR_NONE;
+}
---
+++ grading/projet01/done/memory.c
@@ -0,0 +1,331 @@
+/**
+ * @memory.c
+ * @brief memory management functions (dump, init from file, etc.)
+ *
+ * @date 2018-19
+ */
+
+#if defined _WIN32  || defined _WIN64
+#define __USE_MINGW_ANSI_STDIO 1
+#endif
+
+#include <stdio.h>  // for FILE
+#include <stdlib.h> // for strtoul
+#include <stdint.h> // for size_t
+#include <string.h> // for memset()
+#include <inttypes.h> // for SCNx macros
+#include <assert.h>
+
+#include "memory.h"
+#include "page_walk.h"
+#include "addr_mng.h"
+#include "util.h" // for SIZE_T_FMT
+#include "addr.h" // for virt_addr_t and phy_addr_t
+#include "error.h"
+
+// ======================================================================
+/**
+ * @brief Tool function to print an address.
+ *
+ * @param show_addr the format how to display addresses; see addr_fmt_t type in memory.h
+ * @param reference the reference address; i.e. the top of the main memory
+ * @param addr the address to be displayed
+ * @param sep a separator to print after the address (and its colon, printed anyway)
+ *
+ */
+static void address_print(addr_fmt_t show_addr, const void* reference,
+                          const void* addr, const char* sep)
+{
+    switch (show_addr) {
+    case POINTER:
+        (void)printf("%p", addr);
+        break;
+    case OFFSET:
+        (void)printf("%zX", (const char*)addr - (const char*)reference);
+        break;
+    case OFFSET_U:
+        (void)printf(SIZE_T_FMT, (const char*)addr - (const char*)reference);
+        break;
+    default:
+        // do nothing
+        return;
+    }
+    (void)printf(":%s", sep);
+}
+
+// ======================================================================
+/**
+ * @brief Tool function to print the content of a memory area
+ *
+ * @param reference the reference address; i.e. the top of the main memory
+ * @param from first address to print
+ * @param to first address NOT to print; if less that `from`, nothing is printed;
+ * @param show_addr the format how to display addresses; see addr_fmt_t type in memory.h
+ * @param line_size how many memory byted to print per stdout line
+ * @param sep a separator to print after the address and between bytes
+ *
+ */
+static void mem_dump_with_options(const void* reference, const void* from, const void* to,
+                                  addr_fmt_t show_addr, size_t line_size, const char* sep)
+{
+    assert(line_size != 0);
+    size_t nb_to_print = line_size;
+    for (const uint8_t* addr = from; addr < (const uint8_t*) to; ++addr) {
+        if (nb_to_print == line_size) {
+            address_print(show_addr, reference, addr, sep);
+        }
+        (void)printf("%02"PRIX8"%s", *addr, sep);
+        if (--nb_to_print == 0) {
+            nb_to_print = line_size;
+            putchar('\n');
+        }
+    }
+    if (nb_to_print != line_size) putchar('\n');
+}
+
+// ======================================================================
+// See memory.h for description
+int vmem_page_dump_with_options(const void *mem_space, const virt_addr_t* from,
+                                addr_fmt_t show_addr, size_t line_size, const char* sep)
+{
+#ifdef DEBUG
+    debug_print("mem_space=%p\n", mem_space);
+    (void)fprintf(stderr, __FILE__ ":%d:%s(): virt. addr.=", __LINE__, __func__);
+    print_virtual_address(stderr, from);
+    (void)fputc('\n', stderr);
+#endif
+    phy_addr_t paddr;
+    zero_init_var(paddr);
+
+    M_EXIT_IF_ERR(page_walk(mem_space, from, &paddr),
+                  "calling page_walk() from vmem_page_dump_with_options()");
+
+#ifdef DEBUG
+    (void)fprintf(stderr, __FILE__ ":%d:%s(): phys. addr.=", __LINE__, __func__);
+    print_physical_address(stderr, &paddr);
+    (void)fputc('\n', stderr);
+#endif
+    const uint32_t paddr_offset = ((uint32_t) paddr.phy_page_num << PAGE_OFFSET);
+    const char * const page_start = (const char *)mem_space + paddr_offset;
+    const char * const start = page_start + paddr.page_offset;
+    const char * const end_line = start + (line_size - paddr.page_offset % line_size);
+    const char * const end   = page_start + PAGE_SIZE;
+    debug_print("start=%p (offset=%zX)\n", (const void*) start, start - (const char *)mem_space);
+    debug_print("end  =%p (offset=%zX)\n", (const void*) end, end   - (const char *)mem_space) ;
+    mem_dump_with_options(mem_space, page_start, start, show_addr, line_size, sep);
+    const size_t indent = paddr.page_offset % line_size;
+    if (indent == 0) putchar('\n');
+    address_print(show_addr, mem_space, start, sep);
+    for (size_t i = 1; i <= indent; ++i) printf("  %s", sep);
+    mem_dump_with_options(mem_space, start, end_line, NONE, line_size, sep);
+    mem_dump_with_options(mem_space, end_line, end, show_addr, line_size, sep);
+    return ERR_NONE;
+}
+
+/**
+ * @brief Create and initialize the whole memory space from a provided
+ * (binary) file containing one single dump of the whole memory space.
+ *
+ * @param filename the name of the memory dump file to read from
+ * @param memory (modified) pointer to the begining of the memory
+ * @param mem_capacity_in_bytes (modified) total size of the created memory
+ * @return error code, *memory shall be NULL in case of error
+ *
+ */
+
+int mem_init_from_dumpfile(const char* filename, void** memory, size_t* mem_capacity_in_bytes){
+	FILE* mem_dump = fopen(filename, "rb");
+
+	if(mem_dump != NULL){
+		//Determine file size
+		fseek(mem_dump, 0L, SEEK_END);
+		*mem_capacity_in_bytes = (size_t) ftell(mem_dump);
+		rewind(mem_dump);
+
+		//Allocate memory
+		*memory = calloc(*mem_capacity_in_bytes, 1);
+
+    // correcteur free file when error
+
+
+		size_t bytes_read;
+		if(*memory != NULL){
+			//Initialize the memory from mem_dump file
+			bytes_read = fread(*memory, 1, *mem_capacity_in_bytes, mem_dump);
+		}
+		fclose(mem_dump);
+
+		M_EXIT_IF_NULL(*memory, *mem_capacity_in_bytes);
+		M_REQUIRE(bytes_read == *mem_capacity_in_bytes, ERR_IO,
+              "%s", "Couldn't read the whole memory dump file");
+	}else{
+		*memory = NULL;
+		M_EXIT_ERR(ERR_IO, "Error opening file %s", filename);
+	}
+
+	return ERR_NONE;
+}
+
+
+/**
+ * @brief Create and initialize the whole memory space from a provided
+ * (metadata text) file containing an description of the memory.
+ * Its format is:
+ *  line1:           TOTAL MEMORY SIZE (size_t)
+ *  line2:           PGD PAGE FILENAME
+ *  line4:           NUMBER N OF TRANSLATION PAGES (PUD+PMD+PTE)
+ *  lines5 to (5+N): LIST OF TRANSLATION PAGES, expressed with two info per line:
+ *                       INDEX OFFSET (uint32_t in hexa) and FILENAME
+ *  remaining lines: LIST OF DATA PAGES, expressed with two info per line:
+ *                       VIRTUAL ADDRESS (uint64_t in hexa) and FILENAME
+ *
+ * @param filename the name of the memory content description file to read from
+ * @param memory (modified) pointer to the begining of the memory
+ * @param mem_capacity_in_bytes (modified) total size of the created memory
+ * @return error code, *p_memory shall be NULL in case of error
+ *
+ */
+ #define MAX_FILENAME_SIZE 128
+ #define MAX_LINE_LENGTH MAX_FILENAME_SIZE+18
+
+static int page_file_read( const phy_addr_t* phy_addr,
+            						   const char* page_filename,
+            						   void* const memory,
+            						   const size_t mem_capacity_in_bytes);
+
+/**
+ * @brief Convert a virtual address (given as a uint_64_t) to a physical address, using page_walk
+ * @param memory to lookup the mapping
+ * @param vaddr64 the virtual address to convert
+ * @param paddr (modified) the physical address after conversion
+ * @return ERR_NONE if successful, appropriate error code otherwise
+ */
+static int virt_uint_64_to_phy_addr(void * const memory,
+									const uint64_t vaddr64,
+									phy_addr_t * const paddr);
+
+int mem_init_from_description(const char* master_filename,
+                              void** memory,
+                              size_t* mem_capacity_in_bytes) {
+
+	FILE* f = fopen(master_filename, "r");
+	M_REQUIRE_NON_NULL_CUSTOM_ERR(f, ERR_IO);
+
+/*
+ *  line1:           TOTAL MEMORY SIZE (size_t)
+ *  line2:           PGD PAGE FILENAME
+ *  line4:           NUMBER N OF TRANSLATION PAGES (PUD+PMD+PTE)
+ *  lines5 to (5+N): LIST OF TRANSLATION PAGES, expressed with two info per line:
+ *                       INDEX OFFSET (uint32_t in hexa) and FILENAME
+ *  remaining lines: LIST OF DATA PAGES, expressed with two info per line:
+ *                       VIRTUAL ADDRESS (uint64_t in hexa) and FILENAME
+ */
+
+	/*** ALLOCATE THE WHOLE PHYSICAL MEMORY SPACE (FOR ALL THE PAGES) ***/
+	fscanf(f, "%zu", mem_capacity_in_bytes);
+	*memory  = calloc(*mem_capacity_in_bytes, 1);
+
+	if(*memory == NULL) { fclose(f); return ERR_MEM; }
+
+	/*** WRITE THE TRANSLATION PAGES IN MEMORY ***/
+	char filename[MAX_FILENAME_SIZE];
+	phy_addr_t paddr;
+	uint32_t paddr_32b = 0u;
+	error_code err = ERR_NONE;
+
+	// PGD
+	fscanf(f, "%s", filename);
+	if((err = init_phy_addr(&paddr, 0, 0)) != ERR_NONE) { //PGD starts at phy addr. 0
+    fclose(f); return err;
+  }
+	if(page_file_read(&paddr, filename, *memory, *mem_capacity_in_bytes) != ERR_NONE){
+    fclose(f); return ERR_IO;
+  }
+
+  // OTHER TRANSLATION PAGES
+	int nb_pages;
+	fscanf(f, "%d", &nb_pages);
+
+	uint64_t vaddr64 = 0ul;
+	for(int i = 0; i < nb_pages; i++) {
+		fscanf(f, "%x %s", &paddr_32b, filename);
+		if((err = init_phy_addr(&paddr, paddr_32b, 0)) != ERR_NONE) {
+        	fclose(f);
+        	return err;
+    	}
+		if(page_file_read(&paddr, filename, *memory, *mem_capacity_in_bytes) != ERR_NONE) {
+      		fclose(f);
+     		return ERR_IO;
+    	}
+  	}
+
+  /*** WRITE DATA PAGES ***/
+	char newline[MAX_LINE_LENGTH+1];
+	while(fgets(newline, MAX_LINE_LENGTH, f) != NULL) {
+		fscanf(f, "%lx %s", &vaddr64, filename);
+		if((err = virt_uint_64_to_phy_addr(*memory, vaddr64, &paddr)) != ERR_NONE) {
+      		fclose(f);
+      		return err;
+    	}
+		if(page_file_read(&paddr, filename, *memory, *mem_capacity_in_bytes) != ERR_NONE) {
+      		fclose(f);
+      		return ERR_IO;
+    	}
+	}
+	fclose(f);
+	return ERR_NONE;
+}
+
+/**
+ * @brief Translates a virtual address, given as a 64-bit value, in the corresponding physical address 
+ */
+static int virt_uint_64_to_phy_addr(void * const memory, const uint64_t vaddr64, phy_addr_t * paddr){
+	virt_addr_t vaddr;
+
+	if(init_virt_addr64(&vaddr, vaddr64) != ERR_NONE) { return ERR_ADDR; }
+	if(page_walk(memory, &vaddr, paddr) != ERR_NONE) { return ERR_MEM; }
+
+	return ERR_NONE;
+}
+
+/**
+ * @brief Reads the data from a page file and puts it in memory at given address
+ * @param phy_addr physical address in memory, where to write the page
+ * @param page_filename the file name of the page to read the data from
+ * @param memory the memory space to write the page in
+ * @param mem_capacity_in_bytes the memory capacity, to ensure the is enough space
+ * @return ERR_NONE if sucessful, appropriate error code otherwise
+ */ 
+static int page_file_read(
+	const phy_addr_t* phy_addr,
+	const char* page_filename,
+	void* const memory,
+	const size_t mem_capacity_in_bytes) {
+
+	FILE* page_file = fopen(page_filename, "rb");
+	M_REQUIRE_NON_NULL_CUSTOM_ERR(page_file, ERR_IO);
+
+	//Determine page file size
+	fseek(page_file, 0L, SEEK_END); // correcteur: see PAGE_SIZE
+	size_t page_file_size = ftell(page_file);
+	rewind(page_file);
+
+	size_t bytes_read;
+	uint32_t phy_addr_32b = phy_addr_t_to_uint32_t(phy_addr);
+
+  // correcteur what if phy addr 32b is not alligned to the beginning of a page?
+
+	//Check that it fits in allocated memory from given address
+	if(phy_addr_32b + page_file_size <= mem_capacity_in_bytes){
+		//Read the page_file and write it in memory
+		bytes_read = fread(&((byte_t*)memory)[phy_addr_32b], 1, page_file_size, page_file);
+	}else{
+		fclose(page_file);
+		M_EXIT_ERR(ERR_MEM, "%s", "Not enough space to store the whole page file in memory from given physical address");
+	}
+
+	fclose(page_file);
+	M_REQUIRE(bytes_read == page_file_size, ERR_IO, "%s", "Couldn't read the whole page file");
+
+	return ERR_NONE;
+}
