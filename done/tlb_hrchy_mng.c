b227caabbeae7c7f0f6651a4155a56353c501304
Leo Tafti <leotafti@gmail.com>
2019-05-12 11:21:04 +0200

    Hopefully fixed eviction

--- done/tlb_hrchy_mng.c
+++ done/tlb_hrchy_mng.c
@@ -252,27 +252,47 @@ int tlb_entry_init( const virt_addr_t * vaddr,
         uint8_t index = index_from_tag_and_index(tag_and_index, TLB_LINES); \
         tlb_insert(index, &new_entry, tlb, TLB_TYPE); \
     } while(0)
-/* FIXME eviction policy not functional
-cf forum post
-idea seems right (discussed it with others for whom it works),execution must be wrong
-most likely computation of eviction_tag_and_index
 
-if L2 entry is valid
-    recover L1 tag from L2 tag and index
-    if other L1 contains something for this tag
-      evict
-*/
-// TODO less useless variables?
-#define INVALIDATE_IF_NECESSARY(tlb, TLB_LINES) \
+//TODO : see if this passes make feedback
+//TODO : this is almost the same as INSERT just above with params to insert in L2 tlb, except for the INVALIDATE_IF_NECESSARY call.
+//      I do not see a clean way of merging these two to avoid code duplication, other than passing an additional boolean value
+//      and do something like :
+//      if(must_invalidate){
+//            INVALIDATE_IF_NECESSARY(...)
+//      }
+//      It would be more compact, but less readable and makes no sense when we simply want to insert in a L1 tlb
+
+#define INSERT_L2_AND_INVALIDATE_L1(l1_tlb, L1_TLB_LINES) \
     do { \
+        /*Create and init. a new L2 tlb entry*/ \
+        l2_tlb_entry_t new_entry; \
+        tlb_entry_init(vaddr, paddr, &new_entry, L2_TLB); \
         uint64_t tag_and_index = tag_and_index_from_vaddr(vaddr); \
-        uint8_t l2_index = index_from_tag_and_index(tag_and_index, L2_TLB_LINES); \
-        if(l2_tlb[l2_index].v == VALID) { \
-          uint32_t eviction_tag = l2_tlb[l2_index].tag; \
-          uint64_t eviction_tag_and_index = (eviction_tag * L2_TLB_LINES) + l2_index;\
-          uint32_t l1_tag = tag_from_tag_and_index(eviction_tag_and_index, TLB_LINES);\
-          uint8_t  l1_index = index_from_tag_and_index(eviction_tag_and_index, TLB_LINES); \
-          if(tlb[l1_index].tag == l1_tag){ tlb[l1_index].v = INVALID; } \
+        uint8_t index = index_from_tag_and_index(tag_and_index, L2_TLB_LINES); \
+        \
+        /*If l2 entry was also in the other l1 tlb, invalidate it*/ \
+        INVALIDATE_IF_NECESSARY(l1_dtlb, L1_DTLB_LINES); \
+        \
+        /*Finally, insert the new entry both in the L2 and the INSTRUCTION tlbs*/ \
+        tlb_insert(index, &new_entry, l2_tlb, L2_TLB); \
+    } while(0)
+
+
+#define INVALIDATE_IF_NECESSARY(l1_tlb, L1_TLB_LINES) \
+    do { \
+        /*Get the previous entry in L2 TLB*/\
+        l2_tlb_entry_t old_entry = l2_tlb[index];\
+        if(old_entry.v == VALID){ \
+            \
+            /*Concat the l2 entry tag and index*/\
+            uint64_t old_l2_tag_and_index = (old_entry.tag << L2_TLB_LINES_BITS) | index;\
+            \
+            /*Split again into l1 entry tag and index*/\
+            uint32_t old_l1_tag = tag_from_tag_and_index(old_l2_tag_and_index, L1_TLB_LINES);\
+            uint8_t old_l1_index = index_from_tag_and_index(old_l2_tag_and_index, L1_TLB_LINES);\
+            \
+            /*Search the l1 tlb and invalidate if tag matches*/\
+            if(l1_tlb[old_l1_index].tag == old_l1_tag){ l1_tlb[old_l1_index].v = INVALID; }\
         } \
     } while(0)
 
@@ -292,6 +312,7 @@ int tlb_search( const void * mem_space,
     M_REQUIRE_NON_NULL(l1_dtlb);
     M_REQUIRE_NON_NULL(l2_tlb);
     M_REQUIRE_NON_NULL(hit_or_miss);
+
     //Search in appropriate L1 TLB
     switch (access)
     {
@@ -333,12 +354,12 @@ int tlb_search( const void * mem_space,
             switch (access)
             {
             case INSTRUCTION:
+                INSERT_L2_AND_INVALIDATE_L1(l1_dtlb, L1_DTLB_LINES);
                 INSERT(l1_itlb_entry_t, L1_ITLB, L1_ITLB_LINES, l1_itlb);
-                INVALIDATE_IF_NECESSARY(l1_dtlb, L1_DTLB_LINES);
                 break;
             case DATA:
+                INSERT_L2_AND_INVALIDATE_L1(l1_itlb, L1_ITLB_LINES);
                 INSERT(l1_dtlb_entry_t, L1_DTLB, L1_DTLB_LINES, l1_dtlb);
-                INVALIDATE_IF_NECESSARY(l1_itlb, L1_ITLB_LINES);
                 break;
             default:
                 M_EXIT(ERR_BAD_PARAMETER, "%s", "Unrecognized memory access type");
@@ -354,4 +375,5 @@ int tlb_search( const void * mem_space,
 }
 
  #undef INSERT
+ #undef INSERT_L2_AND_INVALIDATE_L1
  #undef INVALIDATE_IF_NECESSARY
